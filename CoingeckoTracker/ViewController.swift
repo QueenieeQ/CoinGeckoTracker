963b92f69adf7afea9b234aa11df45064c05c0f5
QueenieeQ <xuanquyhnvn@gmail.com>
2023-04-06 01:08:46 +0700

    SwiftUI

--- CoingeckoTracker/ViewController.swift
+++ CoingeckoTracker/ViewController.swift
@@ -8,7 +8,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var btcPrice: UILabel!
+    @IBOutlet weak var ethPrice: UILabel!
+    
+    @IBOutlet weak var lastUpdated: UILabel!
+    @IBOutlet weak var vndPrice: UILabel!
+    @IBOutlet weak var usdPrice: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
